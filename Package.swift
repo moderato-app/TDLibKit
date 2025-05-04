ff60234e38934b2226a79acbdbe63b1824872329
purr100 <purr@moderato.app>
2025-05-04 19:59:04 +0800

    TDLibFramework 1.8.48-b6303f0c

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.47-971684a3")),
+        .package(url: "https://github.com/Swiftgram/TDLibFramework", .exact("1.8.48-b6303f0c")),
     ],
     targets: [
         .target(
